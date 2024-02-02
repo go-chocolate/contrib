be33aa9d0acfed7b28c918227b946cbdf3aa06ce
Xingchen <laixingchen@aosign.cn>
2024-02-02 10:57:40 +0800

    update kv

--- kv/storage.go
+++ kv/storage.go
@@ -30,3 +30,28 @@ func MustNew(c Config) Storage {
 	}
 	return storage
 }
+
+type prefixStorage struct {
+	prefix  string
+	storage Storage
+}
+
+func Prefix(prefix string, storage Storage) Storage {
+	return &prefixStorage{prefix: prefix, storage: storage}
+}
+
+func (s *prefixStorage) Get(ctx context.Context, key string) ([]byte, error) {
+	return s.storage.Get(ctx, s.prefix+key)
+}
+
+func (s *prefixStorage) Set(ctx context.Context, key string, val []byte, expiration ...time.Duration) error {
+	return s.storage.Set(ctx, s.prefix+key, val, expiration...)
+}
+
+func (s *prefixStorage) Del(ctx context.Context, keys ...string) error {
+	var ks = make([]string, 0, len(keys))
+	for i := range keys {
+		ks = append(ks, s.prefix+keys[i])
+	}
+	return s.storage.Del(ctx, ks...)
+}
