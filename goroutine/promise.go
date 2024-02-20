a58c382b60b9c1b4766c66d680e617a5474de409
Xingchen <laixingchen@aosign.cn>
2024-02-20 11:17:05 +0800

    goroutine

--- goroutine/promise.go
+++ goroutine/promise.go
@@ -1,41 +1,134 @@
 package goroutine
 
-import "sync"
+import (
+	"context"
+	"fmt"
+	"sync"
+)
 
-type Promisor[INPUT, OUTPUT any] interface {
-	Then(f func(OUTPUT, error)) Promisor[INPUT, OUTPUT]
-	Await() (OUTPUT, error)
+type Waiter interface {
+	Wait()
 }
 
-type promise[INPUT, OUTPUT any] struct {
-	f      func(INPUT) (OUTPUT, error)
-	output OUTPUT
-	err    error
-	wait   *sync.WaitGroup
+type Promisor[OUTPUT any] interface {
+	Then(ctx context.Context, then func(OUTPUT, error)) Waiter
+	Await(ctx context.Context) (OUTPUT, error)
 }
 
-func (p *promise[INPUT, OUTPUT]) do(input INPUT) {
-	p.wait = new(sync.WaitGroup)
-	p.wait.Add(1)
-	ech := make(chan error, 1)
-	safeGo(func() { p.output, p.err = p.f(input) }, p.wait, ech)
-	if err, ok := <-ech; ok {
-		p.err = err
+type promise[OUTPUT any] struct {
+	ch  chan OUTPUT
+	ech chan error
+}
+
+func (p *promise[OUTPUT]) Then(ctx context.Context, then func(OUTPUT, error)) Waiter {
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	Go(func() {
+		defer wg.Done()
+		then(p.Await(ctx))
+	})
+	return wg
+}
+
+func (p *promise[OUTPUT]) Await(ctx context.Context) (OUTPUT, error) {
+	var output OUTPUT
+	select {
+	case output = <-p.ch:
+		return output, nil
+	case e := <-p.ech:
+		return output, e
+	case <-ctx.Done():
+		return output, ctx.Err()
 	}
 }
 
-func (p *promise[INPUT, OUTPUT]) Then(f func(OUTPUT, error)) Promisor[INPUT, OUTPUT] {
-	safeGo(func() { f(p.output, p.err) }, nil, nil)
+func Promise[INPUT, OUTPUT any](f func(INPUT) (OUTPUT, error), input INPUT) Promisor[OUTPUT] {
+	p := &promise[OUTPUT]{
+		ch:  make(chan OUTPUT, 1),
+		ech: make(chan error, 1),
+	}
+	Go(func() {
+		result, err := doPromise(f, input)
+		if err != nil {
+			p.ech <- err
+		} else {
+			p.ch <- result
+		}
+	})
 	return p
 }
 
-func (p *promise[INPUT, OUTPUT]) Await() (OUTPUT, error) {
-	p.wait.Wait()
-	return p.output, p.err
+func doPromise[INPUT, OUTPUT any](f func(INPUT) (OUTPUT, error), input INPUT) (result OUTPUT, err error) {
+	defer func() {
+		if panicInfo := recover(); panicInfo != nil {
+			if e, ok := panicInfo.(error); ok {
+				err = e
+			} else {
+				err = fmt.Errorf("%v", panicInfo)
+			}
+		}
+	}()
+	result, err = f(input)
+	return
 }
 
-func Promise[INPUT, OUTPUT any](f func(INPUT) (OUTPUT, error), input INPUT) Promisor[INPUT, OUTPUT] {
-	p := &promise[INPUT, OUTPUT]{f: f}
-	p.do(input)
+type AnyPromisor interface {
+	Then(ctx context.Context, then func(any, error)) Waiter
+	Await(context.Context) (any, error)
+}
+
+type anyPromise struct {
+	ch  chan any
+	ech chan error
+}
+
+func (p *anyPromise) Then(ctx context.Context, then func(any, error)) Waiter {
+	wg := new(sync.WaitGroup)
+	wg.Add(1)
+	Go(func() {
+		defer wg.Done()
+		then(p.Await(ctx))
+	})
+	return wg
+}
+
+func (p *anyPromise) Await(ctx context.Context) (any, error) {
+	select {
+	case v := <-p.ch:
+		return v, nil
+	case e := <-p.ech:
+		return nil, e
+	case <-ctx.Done():
+		return nil, ctx.Err()
+	}
+}
+
+func AnyPromise(f func(...any) (any, error), args ...any) AnyPromisor {
+	p := &anyPromise{
+		ch:  make(chan any, 1),
+		ech: make(chan error, 1),
+	}
+	Go(func() {
+		result, err := doAnyPromise(f, args...)
+		if err != nil {
+			p.ech <- err
+		} else {
+			p.ch <- result
+		}
+	})
 	return p
+}
+
+func doAnyPromise(f func(...any) (any, error), args ...any) (result any, err error) {
+	defer func() {
+		if panicInfo := recover(); panicInfo != nil {
+			if e, ok := panicInfo.(error); ok {
+				err = e
+			} else {
+				err = fmt.Errorf("%v", panicInfo)
+			}
+		}
+	}()
+	result, err = f(args...)
+	return
 }
--- goroutine/promise_test.go
+++ goroutine/promise_test.go
@@ -1,6 +1,7 @@
 package goroutine
 
 import (
+	"context"
 	"strconv"
 	"testing"
 )
@@ -10,9 +11,16 @@ func Atoi(s string) (int, error) {
 }
 
 func TestPromise(t *testing.T) {
-	t.Log(
-		Promise(Atoi, "1").
-			Then(func(result int, err error) { t.Log(result, err) }).
-			Await(),
-	)
+	Promise(Atoi, "1").Then(context.Background(), func(result int, err error) { t.Log(result, err) }).Wait()
+
+	t.Log(Promise(Atoi, "2").Await(context.Background()))
+}
+
+func TestAnyPromise(t *testing.T) {
+	f := func(a ...any) (any, error) {
+		return Atoi(a[0].(string))
+	}
+	AnyPromise(f, "111").Then(context.Background(), func(result any, err error) { t.Log(result, err) }).Wait()
+
+	t.Log(AnyPromise(f, "222").Await(context.Background()))
 }
