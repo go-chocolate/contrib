9daaaa4e8b2c7819db2aee5413aef2d4705dad6f
Xingchen <laixingchen@aosign.cn>
2024-03-12 15:12:17 +0800

    update

--- database/repository/repository.go
+++ database/repository/repository.go
@@ -1,6 +1,9 @@
 package repository
 
-import "context"
+import (
+	"context"
+	"errors"
+)
 
 type Repository[T any] interface {
 	FindOne(ctx context.Context, where any) (*T, error)
@@ -15,4 +18,30 @@ type Repository[T any] interface {
 type Iterator[T any] interface {
 	Count() (int64, error)
 	Next() ([]*T, error)
+}
+
+var ErrUnimplemented = errors.New("not implemented")
+
+type UnimplementedRepository[T any] struct{}
+
+func (rep *UnimplementedRepository[T]) FindOne(ctx context.Context, where any) (*T, error) {
+	return nil, ErrUnimplemented
+}
+func (rep *UnimplementedRepository[T]) List(ctx context.Context, where any, offset, limit int, order ...any) ([]*T, int64, error) {
+	return nil, 0, ErrUnimplemented
+}
+func (rep *UnimplementedRepository[T]) Count(ctx context.Context, where any) (int64, error) {
+	return 0, ErrUnimplemented
+}
+func (rep *UnimplementedRepository[T]) Update(ctx context.Context, where any, update any) (int64, error) {
+	return 0, ErrUnimplemented
+}
+func (rep *UnimplementedRepository[T]) Delete(ctx context.Context, where any) (int64, error) {
+	return 0, ErrUnimplemented
+}
+func (rep *UnimplementedRepository[T]) Insert(ctx context.Context, data any) (int64, error) {
+	return 0, ErrUnimplemented
+}
+func (rep *UnimplementedRepository[T]) Iterate(ctx context.Context, column string, where any) (Iterator[T], error) {
+	return nil, ErrUnimplemented
 }
